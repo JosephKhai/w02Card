e35e7c793c8a3c892379ac1f6c64f67286514385
Myanmar Vicroad Learner <josephkhaipi@gmail.com>
2021-08-19 17:40:55 +1000

    random button

--- app/src/main/java/com/example/w02card/MainActivity.kt
+++ app/src/main/java/com/example/w02card/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.w02card
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +14,19 @@ class MainActivity : AppCompatActivity() {
         val aceView = findViewById<TextView>(R.id.ace_View)
         aceView.text = card.getDetails()
 
+        //flip button
         val flipButton = findViewById<TextView>(R.id.flip_button)
-
         flipButton.setOnClickListener(){
             card.flip()
             aceView.text = card.getDetails()
+        }
+
+        //random button
+        val randButton = findViewById<Button>(R.id.ran_button)
+        randButton.setOnClickListener(){
+            val card = Card()
+            aceView.text = card.getDetails()
+
         }
 
 
